8f50bf9191b05a8189a6f653ae0878bf1c583c39
abulleDev <olives2739@gmail.com>
2024-09-29 10:15:11 +0900

    feat: Create build.js

---
+++ build.js
@@ -0,0 +1,12 @@
+const packager = require('@electron/packager')
+
+packager({
+  dir: '.',
+  out: './out',
+  appVersion: '1.0.0',
+  icon: './assets/icons/icon',
+  platform: ['win32', 'darwin', 'linux'],
+  arch: ['x64', 'arm64'],
+  overwrite: true,
+  asar: true,
+})
