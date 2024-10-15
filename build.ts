8b32298554f9ace78ab968aa1ba5905889d466aa
abulleDev <olives2739@gmail.com>
2024-10-15 19:03:46 +0900

    refactor: Move assets path

--- build.ts
+++ build.ts
@@ -4,7 +4,7 @@ packager({
   dir: '.',
   out: './out',
   appVersion: '1.0.0',
-  icon: './assets/icons/icon',
+  icon: './src/assets/icons/icon',
   platform: ['win32', 'darwin', 'linux'],
   arch: ['x64', 'arm64'],
   overwrite: true,
